0ab0be5d7cab4ea18cc0f54e599f951508fbb774
Shameem Muhammed Shareef <shemim313@gmail.com>
2023-11-03 01:56:00 +0530

    redis changed to config

--- cargo/settings.py
+++ cargo/settings.py
@@ -105,8 +105,8 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            # "hosts": [config('redis')],
-            "hosts": ["127.0.0.1", 6379],
+            "hosts": [config('redis')],
+            # "hosts": ["127.0.0.1", 6379],
             
         },
     },
